ef6c6d922ee83304adb4fac71cf0889748775729
Duckduckgame <gabriel.h.kenny@gmail.com>
2019-06-18 14:25:09 +0100

    Initial Commit

---
+++ SummerJam19/Assets/Scripts/tileInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileInfo : MonoBehaviour
+{
+
+    enum tileType {Empty, Vine, Flower};
+
+    private void Start()
+    {
+        tileType crntType;
+
+        crntType = tileType.Empty;
+    }
+}
---
+++ SummerJam19/Assets/Scripts/tileScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileScript : MonoBehaviour
+{
+    int mapSizeX = 9;
+    int mapSizeY = 9;
+
+    public GameObject tileGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+
+            }
+
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
